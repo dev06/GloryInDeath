7853ecdc3ebbdb5ede5aa1646d26b83b4a98fc37
Parz1val1 <35855311+Parz1val1@users.noreply.github.com>
2019-09-09 21:06:07 -0400

    Implemented music
    I implemented the music I had based on the state changes in the game.

---
+++ Assets/Scripts/Controllers/SoundManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public AudioSource musicPlayer;
+    private AudioClip menuMusic;
+    private AudioClip gameplayMusic;
+    private AudioClip deathSound;
+    private State current;
+	// Use this for initialization
+	void Start () {
+        menuMusic = Resources.Load<AudioClip>("Music/Dark Dungeon AMBIENT LOOP");
+        gameplayMusic = Resources.Load<AudioClip>("Music/Barren Combat LOOP");
+        deathSound = Resources.Load<AudioClip>("Music/Death PIANO");
+        musicPlayer.loop = true;
+        current = State.GAME;
+	}
+
+    private void Update()
+    {
+        Debug.Log(GameController.state);
+        Debug.Log(current);
+        if((GameController.state == State.CHARACTER_SELECT) && current != State.CHARACTER_SELECT)
+        {
+            EnterMenu();
+            current = GameController.state;
+        }
+        else if ((GameController.state == State.GAME) && current != State.GAME)
+        {
+            EnterGameplay();
+            current = GameController.state;
+        }
+    }
+    public void EnterMenu()
+    {
+        musicPlayer.Stop();
+        musicPlayer.clip = menuMusic;
+        musicPlayer.Play();
+    }
+
+    public void EnterGameplay()
+    {
+        musicPlayer.Stop();
+        musicPlayer.clip = gameplayMusic;
+        musicPlayer.Play();
+    }
+}
