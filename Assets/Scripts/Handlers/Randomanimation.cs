5d587acb689eb1baffabacf561d69b0a4d0c8029
Tyler-Hocker <55661653+Tyler-Hocker@users.noreply.github.com>
2019-09-22 18:57:37 -0400

    Animation changes.
    Started trying to figure out animation changes we need.

---
+++ Assets/Scripts/Handlers/Randomanimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Randomanimation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnStateMachineEnter(Animator animator, int StateMachinePathMash)
+    {
+        animator.SetInteger("AttackID", Random.Range(0, 4));
+    }
+}
